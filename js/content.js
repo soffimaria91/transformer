ab33228a37a622d8721f583eb5bfbb646337753a
tilman42 <tilman.dietrich@gmail.com>
2024-06-03 22:39:41 +0200

    "no change" fixed

--- js/content.js
+++ js/content.js
@@ -22,6 +22,8 @@ DeveloperNameInputElement.setName = function(d, c, g) {
       for (let i = 0; i < words.length; i++) {
         formattedName += words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
       }
+    } else if (namingConvention === 'noChange') {
+      return;
     } else {
       formattedName = d.replace(/\s+/g, '_');
     }
